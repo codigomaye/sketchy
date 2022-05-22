e3698293cfd7ba74c8b8df6f0e50918699be36d2
nichromat <nichromat@outlook.com>
2022-05-22 19:50:27 +0200

    Arrange grid columns and rows

--- app.js
+++ app.js
@@ -1,28 +1,24 @@
-// get argument "gridNumber" with default value 16
 function createGrid(gridNumber = 16) {
-    
-    // create variable "container" as HTMLDOMELEMENT
+
     let container;
 
-    // set "container" value to DOMELEMENT with class "container"
 
     container = document.querySelector(".container");
 
-    // create variable "grid" as HTMLDOMELEMENT
     let grid;
 
-    // set "grid" value to a new DOMELEMENT of type "div"
     grid = document.createElement("div");
 
-    // set "grid" class attribute to "grid"
     grid.classList.add("grid");
 
-    // START loop for i = 0, while i is smaller thant gridNumber, increment i
-    for(let i = 0; i < gridNumber**2; i++){
-        
-            // append a clone of "grid" in "container"
+    for (let i = 0; i < gridNumber ** 2; i++) {
+
         container.appendChild(grid.cloneNode(true));
     }
-    // END loop
+
+    container.setAttribute("style", `grid-template-columns: repeat(${gridNumber}, 1fr);`);
+
 
 }
+
+createGrid();
