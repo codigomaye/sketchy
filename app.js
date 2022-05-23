a4a1d5844e1ffadbd41c4a8920a59fe66216f9b6
nichromat <nichromat@outlook.com>
2022-05-23 13:32:13 +0200

    Add button to create new grids

--- app.js
+++ app.js
@@ -24,14 +24,55 @@ function createGrid(gridNumber) {
 function drawGrid() {
     // create variable "grids" with value Dom element of class "grid"
     let grids = document.querySelectorAll(".grid");
-    
+
     // for each "grid" in "grids"
     grids.forEach((grid) => {
         // when a grid is hovered, chage its background color to gray 
         grid.addEventListener("mouseover", () => grid.setAttribute("style", "background-color: gray"))
     });
-    
+
 }
 
-createGrid(100);
-drawGrid();
+function deleteGrid() {
+
+    let grids = document.querySelector(".container");
+
+    while (grids.firstChild) {
+        grids.removeChild(grids.lastChild);
+    }
+}
+
+function newGrid() {
+
+    let newGrid = document.querySelector(".newGrid");
+
+    // create variable "gridNumber" as number
+    let gridNumber = 0;
+
+    newGrid.addEventListener("click", () => {
+
+        gridNumber = prompt("Insert a number between 1 - 100");
+
+        if (typeof (parseInt(gridNumber)) == 'number' && gridNumber > 0 && gridNumber <= 100) {
+
+            deleteGrid();
+
+            createGrid(gridNumber);
+
+            drawGrid();
+        }
+        else {
+            alert("You have to insert a number between 1 and 100");
+        }
+    })
+
+}
+
+
+function game(){
+    createGrid(16);
+    drawGrid();
+    newGrid();
+}
+
+game();
